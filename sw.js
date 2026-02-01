e341a700f36490440749db17d5a0f53784fbc23a
suiren22 <suiren22@users.noreply.github.com>
2026-02-01 08:56:30 -0500

    이미지 파일명 영문으로 변경 - 엑박 문제 해결

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = 'game-homework-v1';
+const CACHE_NAME = 'game-homework-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './images/nikke.jpg',
+  './images/genshin.jpg',
+  './images/zzz.jpg',
+  './images/endfield.jpg',
+  './images/wuwa.png',
+  './images/7knights.png',
+  './images/bluearchive.png',
+  './images/gfl2.jpg',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
 ];
 
 // Install event
@@ -24,16 +38,13 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request).then(response => {
-          // Check if we received a valid response
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
-          // Clone the response
           const responseToCache = response.clone();
           caches.open(CACHE_NAME)
             .then(cache => {
